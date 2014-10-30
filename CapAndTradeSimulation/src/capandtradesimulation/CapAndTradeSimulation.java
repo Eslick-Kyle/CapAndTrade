02d110842c2ac4b83170d158582e698571562d66
Ben Furner <bfurner27@yahoo.com>
2014-10-30 14:13:12 -0600

    The first commit ever

---
+++ CapAndTradeSimulation/src/capandtradesimulation/CapAndTradeSimulation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capandtradesimulation;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class CapAndTradeSimulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
