2ba6066926711b00ac17dd2c0f6392cee9766225
Kyle Eslick <mii756@gmail.com>
2014-12-11 12:14:45 -0700

    added setSinglePlayerPowerStationNames() function to the simulation

--- CapAndTradeSimulation/src/model/Simulation.java
+++ CapAndTradeSimulation/src/model/Simulation.java
@@ -10,37 +10,40 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Simulation class holds all the main methods to interact with the data in the 
+ * Simulation class holds all the main methods to interact with the data in the
  * model. It also contains the data to run multiple rounds of the simulation
- * 
+ *
  * @author Benjamin
  */
 public class Simulation {
+
     private List<List<Trade>> tradeHistory = new LinkedList<>();
     private List<PowerStation> powerStations;
     private List<List<Integer>> marginalProfitHistory = new LinkedList<>();
     private int numberOfRounds;
-    
+
     /**
      * Description: default constructor, automatically gives teams names
      */
-    public Simulation () {
+    public Simulation() {
         powerStations = new ArrayList<>();
         numberOfRounds = 0;
     }
-    
+
     /**
-     * Description: constructor takes number of Teams, sets names
-     * to default values
+     * Description: constructor takes number of Teams, sets names to default
+     * values
+     *
      * @param numTeams number of teams
      */
-    public Simulation (int numTeams) {
+    public Simulation(int numTeams) {
         powerStations = new ArrayList<>();
         setPowerStationNamesDefault(numTeams);
     }
-    
+
     /**
      * Description getter for array list
+     *
      * @return this is a list of power stations
      */
     public List<PowerStation> getPowerStations() {
@@ -49,6 +52,7 @@ public class Simulation {
 
     /**
      * The number of rounds played in the simulation
+     *
      * @return number of rounds
      */
     public int getNumberOfRounds() {
@@ -58,11 +62,12 @@ public class Simulation {
     public List<List<Trade>> getTradeHistory() {
         return tradeHistory;
     }
-    
+
     /**
      * Description: names power stations with letters starting at 'a' until the
      * number of teams is reached
-     * @param numberOfTeams 
+     *
+     * @param numberOfTeams
      */
     public void setPowerStationNamesDefault(int numberOfTeams) {
         List<Integer> margeProfit = new ArrayList<>();
@@ -76,13 +81,36 @@ public class Simulation {
         }
         marginalProfitHistory.add(margeProfit);
     }
-    
+/**
+     * Description: Sets user's name to player and adds names for computers
+     * names computer power stations with letters starting at 'a' 
+     *
+     * @param numberOfTeams
+     */
+    public void setSinglePlayerPowerStationNames() {
+        PowerStation team = new PowerStation();
+        team.setPowerStationName("Player");
+        powerStations.add(team);
+        List<Integer> margeProfit = new ArrayList<>();
+        char name = 'A';
+        margeProfit.add(0);
+        for (int i = 0; i < 10; i++) {
+            team = new PowerStation();
+            team.setPowerStationName("Computer " + String.valueOf(name));
+            powerStations.add(team);
+            name++;
+            margeProfit.add(0);
+        }
+        marginalProfitHistory.add(margeProfit);
+    }
+
     /**
-     * Description: takes a list of names and sets the powerStations names to 
+     * Description: takes a list of names and sets the powerStations names to
      * those values
+     *
      * @param powerStationNames - This is the names of the teams
      */
-    public void setPowerStationNames (List<String> powerStationNames) {
+    public void setPowerStationNames(List<String> powerStationNames) {
         List<Integer> margeProfit = new ArrayList<>();
         for (String name : powerStationNames) {
             PowerStation team = new PowerStation();
@@ -92,48 +120,50 @@ public class Simulation {
         }
         marginalProfitHistory.add(margeProfit);
     }
-    
+
     /**
-     * This returns a list of the total marginal profit that was found over 
-     * the course of multiple rounds.
+     * This returns a list of the total marginal profit that was found over the
+     * course of multiple rounds.
+     *
      * @return list of Integers with the total marginal profit
      */
     public List<Integer> getTotalMarginalProfit() {
         /* This section is to format the total marginal profit to be the same
-        size as the number of teams playing */
+         size as the number of teams playing */
         List<Integer> totalMarginalProfit = new LinkedList<>();
         for (List<Integer> mp : marginalProfitHistory) {
             for (int i = 0; i < mp.size(); i++) {
-                totalMarginalProfit.add(0);  
+                totalMarginalProfit.add(0);
             }
             break;
         }
-        
+
         // Here we will find the total marginal profit for all rounds
         for (List<Integer> mp : marginalProfitHistory) {
             int i = 0;
             for (Integer profit : mp) {
                 int temp = totalMarginalProfit.get(i) + profit;
-                totalMarginalProfit.set(i, temp);    
+                totalMarginalProfit.set(i, temp);
                 i++;
-            }         
+            }
         }
         return totalMarginalProfit;
     }
-    
+
     /**
      * Returns the last rounds marginal profit
-     * @return 
+     *
+     * @return
      */
     public List<Integer> getCurrentRoundMarginalProfit() {
         return marginalProfitHistory.get(marginalProfitHistory.size() - 1);
     }
-    
+
     /**
      * Description: This function will take a list of trades made by the users
-     * and will assign that information to the PowerStations. The size of the 
+     * and will assign that information to the PowerStations. The size of the
      * array has to be the same size for this function to work.
-     * 
+     *
      * @param trades - the trades each station made and the price they received
      */
     public void setPowerStationsTradeInformation(List<Trade> trades) {
@@ -143,7 +173,7 @@ public class Simulation {
             System.exit(0);
         }
         ArrayList<Integer> marginalProfit = new ArrayList<>();
-        
+
         // set the trade values in the power stations
         int i = 0;
         for (Trade theTrade : trades) {
@@ -156,7 +186,7 @@ public class Simulation {
         numberOfRounds++;
         marginalProfitHistory.add(marginalProfit);
     }
-    
+
     public void restartSimulation() {
         tradeHistory = new LinkedList<>();
         powerStations = new ArrayList<>();
